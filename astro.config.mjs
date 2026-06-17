3c77db88c3834f072a2bdc91b6077c35676c5a40
Markitsu <2411kjn@gmail.com>
2026-06-17 18:46:38 +0700

    fix: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: "https://markporamest.github.io",
+  base: "/portfolio",
+  integrations: [tailwind()],
 });
