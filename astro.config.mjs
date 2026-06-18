9661879542ca203a5ec8ca4eda3e3aaa52cfdb6e
Markitsu <2411kjn@gmail.com>
2026-06-18 16:44:02 +0700

    feat: add openCertModal for skill page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://markporamest.github.io",
-  base: "/portfolio/",
+  base: "/portfolio",
   integrations: [tailwind()],
 });
